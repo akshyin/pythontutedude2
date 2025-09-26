d2f9e2042199622624f26d934b7fb68f4d3c2036
akshyin <akshay.akshyin@gmail.com>
2025-09-26 08:30:02 +0530

    formatted output of task one

--- taskOne.py
+++ taskOne.py
@@ -1,7 +1,7 @@
 #Take an indicator for error happened in input
 errorfound=False
 #Take the number as input from user and assign it to number variable
-number=input("Enter the integer: ")
+number=input("Enter a number: ")
 #Convert number into integer
 try:
     number=int(number)
@@ -11,6 +11,6 @@ except ValueError:
 
 if not errorfound:
     if(number%2==0):
-        print(number," is even number")
+        print(number," is even number.")
     else:
-        print(number," is odd number")
+        print(number," is odd number.")
