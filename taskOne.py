9a20a3ad61a3a848d5f6fc2ed9e19fd7745f1caa
akshyin <akshay.akshyin@gmail.com>
2025-09-26 08:22:32 +0530

    first version of the program completed

---
+++ taskOne.py
@@ -0,0 +1,18 @@
+#Take an indicator for error happened in input
+errorfound=False
+#Take the number as input from user and assign it to number variable
+number=input("Enter the integer: ")
+#Convert number into integer
+try:
+    number=int(number)
+except ValueError:
+    errorfound=True
+    print("Error in reading number")
+
+if not errorfound:
+    if(number<0):
+        print("Only positive number allowed")
+    elif(number%2==0):
+        print(number," is even number")
+    else:
+        print(number," is odd number")
---
+++ taskTwo.py
@@ -0,0 +1,8 @@
+#initialize a variable summation as zero
+summation =0
+#the for loop start with 1 and end with 50, excluding 51
+for i in range(1,50+1):
+    #adding i in each loop to the summation
+    summation = summation+i
+#printing the output
+print("The sum of numbers from 1 to 50 is: ",summation);
